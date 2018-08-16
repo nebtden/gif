b9017f7dfdf2ed42e7ddeec82a9a324499bafaef
simon <nebtden@gmail.com>
2018-08-16 16:35:40 +0800

    gif 转换

--- index.py
+++ index.py
@@ -2,9 +2,17 @@ from PIL import Image, ImageSequence
 import numpy as np
 # import changecolor
 import os, sys
+argv = sys.argv
+import time
+from shushan import post_file
 
+basename = argv[1]
+print(basename)
 
-im = Image.open("images/lei.gif")
+filename = 'original/'+basename+'.gif'
+new_filename = 'gif/'+basename+'.gif'
+
+im = Image.open(filename)
 # GIF图片流的迭代器
 iter = ImageSequence.Iterator(im)
 
@@ -12,35 +20,43 @@ iter = ImageSequence.Iterator(im)
 # 清空之前生成的图片
 def changecolor(arr):
     size = arr.shape
-    for i in range(size[0]):
-        for j in range(size[1]):
-            r, g, b = arr[i, j]
-            if r == g == b:
-                continue
-            elif r >= 60 and r <= 150:
-                arr[i, j] = [4, 4, 4]
+    #直接根据大小更改
+    if size[0] == 480:
+        for i in range(size[0]):
+            for j in range(size[1]):
+                r, g, b = arr[i, j]
+                if r == g == b:
+                    continue
+                else:
+                    arr[i, j] = [0, 0, 0]
+    else:
+        for i in range(size[0]):
+            for j in range(size[1]):
+                r, g, b = arr[i, j]
+                if r == g == b:
+                    continue
+                elif r >= 60 and r <= 150:
+                    arr[i, j] = [4, 4, 4]
 
-            elif r == 247 and g == 247 and b == 239:
-                continue
-            else:
-                pass
+                elif r == 247 and g == 247 and b == 239:
+                    continue
+                else:
+                    pass
     img = Image.fromarray(arr)
     # img.show()
     img.save(file)
     return arr
 
 # 列出目录
-# print("目录为: %s" % os.listdir('./out'))
-# for file in os.listdir('./out'):
-#     os.remove('./out/'+file)
+for file in os.listdir('./out'):
+    os.remove('./out/'+file)
+
+time.sleep(3)
 
 imgs = []
 index = 1
 # 遍历图片流的每一帧
 for frame in iter:
-    # print("image %d: mode %s, size %s" % (index, frame.mode, frame.size))
-    # print(frame.mode)
-    # frame.convert("RGB")
 
     img = frame.convert('RGB')
     frame.save("./out/%d.png" % index)
@@ -48,7 +64,9 @@ for frame in iter:
     index += 1
 
 a = os.listdir('./out')
-a.sort()
+# a.sort(key=lambda x : int(x[:-4]))
+a.sort(key=lambda x:int(x[:-4]))
+# a = sorted(a, key=lambda x: int(x[:-4]), reverse=True)
 print(a)
 for file in a:
     file = './out/'+file
@@ -61,4 +79,7 @@ for file in a:
 
 
 # 把图片流重新成成GIF动图
-imgs[0].save('out.gif', save_all=True, append_images=imgs[1:])
+imgs[0].save(new_filename, save_all=True, append_images=imgs[1:])
+time.sleep(1)
+result = post_file(new_filename)
+print(result)
